a784c9eb7b8a8a36f38606555bd1fe7388b72046
Aditya <aditya.novadianto@gmail.com>
2024-07-27 20:28:11 +0700

    add secret

--- speech_to_text/demo1.py
+++ speech_to_text/demo1.py
@@ -38,12 +38,12 @@ def transcribe_audio(uploaded_file):
 
     response = client.recognize(config=config, audio=audio)
 
-    # mongo_uri = st.secrets["MONGO_URI"]
-    # if not mongo_uri:
-    #     raise ValueError("MONGO_URI environment variable not set")
+    mongo_uri = st.secrets["MONGO_URI"]
+    if not mongo_uri:
+        raise ValueError("MONGO_URI environment variable not set")
 
     # MongoDB connection
-    client = MongoClient("mongodb+srv://adityanovadianto:aditNovadianto@cluster0.x9fuvep.mongodb.net/aury?retryWrites=true&w=majority&appName=Cluster0")  # Replace with your MongoDB URI
+    client = MongoClient(mongo_uri)  # Replace with your MongoDB URI
     db = client['aury']  # Replace with your database name
     collection = db['data-transcript']  # Replace with your collection name
 
